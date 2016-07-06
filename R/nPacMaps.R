6b8e7f809d1947d18903b8f89313f2c5257c9f75
Josh London <josh.london@noaa.gov>
2016-07-06 10:30:50 -0700

    ADD STARTUP MESSAGE TO PACKAGE

--- R/nPacMaps.R
+++ R/nPacMaps.R
@@ -1,5 +1,20 @@
-#' Map data (and a few functions) for the North Pacific
+#' North Pacific Maps
 #'
 #' @docType package
 #' @name nPacMaps
 NULL
+
+.onAttach <- function(library, pkgname)
+{
+  info <- utils::packageDescription(pkgname)
+  package <- info$Package
+  version <- info$Version
+  date <- info$Date
+  packageStartupMessage(
+    paste(paste(package, version, paste0("(",date, ")"), "\n"), 
+          "The nPacMaps package requires an additional installation step.\n",
+          "Please type 'install_gshhg()' to complete this install.\n"
+          )
+  )
+  
+}
