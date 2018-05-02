5bea5e6cf6531fa504eed83fa7b97d0ebb1cd93c
Josh M. London <josh.london@noaa.gov>
2018-05-02 09:19:10 -0700

    update to GSHHG 2.3.7

--- R/nPacMaps.R
+++ R/nPacMaps.R
@@ -13,11 +13,11 @@ NULL
   if (dir.exists(system.file("extData", package = "nPacMaps"))) {
     data_path <- system.file("extData", package = "nPacMaps")
   }
-  if (!file.exists(paste(data_path, "gshhg-bin-2.3.6", sep = "/"))) {
+  if (!file.exists(paste(data_path, "gshhg-bin-2.3.7", sep = "/"))) {
   packageStartupMessage(
     paste(paste(package, version, paste0("(",date, ")"), "\n"), 
           "The nPacMaps package requires an additional installation step.\n",
-          "Please type 'install_gshhg()' to install GSHHG v 2.3.6.\n"
+          "Please type 'install_gshhg()' to install GSHHG v 2.3.7.\n"
           )
   )
   }
